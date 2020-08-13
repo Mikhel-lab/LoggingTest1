bb2d427d7a8a0de1ab7e223f2daa123a8f7afef6
Michael Aiyetan <michael.aiyetanjnr03@gmail.com>
2020-08-13 00:30:43 -0700

    Add project files.

---
+++ LoggingTest1/Myclass.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingTest1
+{
+	public class Myclass
+	{
+
+		private readonly ILogger _logger;
+		public Myclass(ILogger<Myclass> logger)
+		{
+			_logger = logger;
+
+		}
+		internal void Run()
+		{
+			_logger.LogInformation("Application Started at {dateTime}", DateTime.UtcNow);
+
+			//Business Logic START
+			//Business logic END
+			_logger.LogInformation("Application Ended at {dateTime}", DateTime.UtcNow);
+		}
+
+		//private readonly ILogger _logger;
+
+		//public Myclass(ILogger<Myclass> logger)
+		//{
+		//	_logger = logger;
+		//}
+
+		//public void SomeMethod()
+		//{
+		//	_logger.LogInformation("Hello");
+	}
+	
+}
---
+++ LoggingTest1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingTest1
+{
+	class Program
+	{
+		
+
+		static void Main(string[] args)
+		{
+			var services = new ServiceCollection();
+			ConfigureServices(services);
+			using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+			{
+				Myclass app = serviceProvider.GetService<Myclass>();
+				// Start up logic here
+				app.Run();
+			}
+		}
+		private static void ConfigureServices(ServiceCollection services)
+		{
+			services.AddLogging(configure => configure.AddConsole())
+			.AddTransient<Myclass>();
+		}
+	}
+}
